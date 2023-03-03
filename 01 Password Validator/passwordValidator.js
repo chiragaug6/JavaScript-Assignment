26aceb6c8d66bfcb7d3633c18825be95890c29f6
Chirag Solanki <104585228+Chirag-Solanki6804@users.noreply.github.com>
2023-03-03 23:44:41 +0530

    Add files via upload

---
+++ 01 Password Validator/passwordValidator.js
@@ -0,0 +1,12 @@
+// Write a JavaScript program that checks if the entered password matches the confirmed password. If the 
+// passwords match, the program should log "Password Matched. Password validation Successful." to the console. 
+// Otherwise, it should log "Password didn't match. Password validation unsuccessful" to the console
+
+const password = 3098;
+const confirmedPassword = 3098;
+
+if (password === confirmedPassword) {
+    console.log("Password Matched. Password validation Successful");
+} else {
+    console.log("Password didn't match. Password validation unsuccessful");
+}
