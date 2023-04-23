8f4ff26b88dc313299dd802cfc32eca48106f60d
chirag solanki <104585228+Chirag-Solanki6804@users.noreply.github.com>
2023-04-23 17:50:51 +0530

    Update calculator.js

--- 03 MileStone-1 JavaScript_Assignment/02 Calculator/calculator.js
+++ 03 MileStone-1 JavaScript_Assignment/02 Calculator/calculator.js
@@ -1,41 +1,42 @@
-// Create a javascript program that takes in two numbers and a string operator. Make use of a switch statement 
-// to perform the operation on the two numbersK
-// The calculator function should:
-// | Take in two numbers, num1 and num2, and a string representing a mathematical operator, operatorJ
-// | Use a switch statement to determine which operation to perform based on the value of the operatorJ
-// | If the operator is one of the four valid operators (+, -, *, /), perform the corresponding mathematical 
-// operation and store the result in a variable called resultJ
-// | If the operator is not one of the valid operators, log "Invalid operator" to the 
+/** 
+ * Create a javascript program that takes in two numbers and a string operator. Make use of a switch statement 
+ * to perform the operation on the two numbersK
+ * The calculator function should:
+ * Take in two numbers, num1 and num2, and a string representing a mathematical operator, operatorJ
+ * Use a switch statement to determine which operation to perform based on the value of the operatorJ
+ * If the operator is one of the four valid operators (+, -, *, /), perform the corresponding mathematical 
+ * operation and store the result in a variable called resultJ
+ * If the operator is not one of the valid operators, log "Invalid operator" to the 
+*/
 
 
-let num1 = 10;
-let num2 = 12;
-let operation = "-";
-let result;
 
-switch (operation) {
-    case "+":
-        result = num1 + num2;
-        break;
-    case "-":
-        result = num1 - num2;
-        break;
-    case "*":
-        result = num1 * num2;
-        break;
-    case "/":
-        result = num1 / num2;
-        break;
+function basicOpration(op) {
+    const num1 = document.getElementById("numberOne").value;
+    const num2 = document.getElementById("numberTwo").value;
 
-    default:
-        console.log("Invalid operator");
-        break;
+    let result;
+    switch (op) {
+        case "plus":
+            result = parseFloat(num1) + parseFloat(num2);
+            break;
+        case "minus":
+            result = parseFloat(num1) - parseFloat(num2);
+            break;
+        case "multi":
+            result = parseFloat(num1) * parseFloat(num2);
+            break;
+        case "div":
+            result = parseFloat(num1) / parseFloat(num2);
+            break;
+
+        default:
+            console.log("Invalid operator");
+            break;
+    }
+
+    output.innerText = `Ans : ${result.toFixed(3)}`;
+    output.style.color = "grey";
+    output.style.fontSize = "2rem";
+    output.style.textAlign = "center"
 }
-
-if (result != null) {
-    console.log(num1 + " " + operation + " " + num2 + " = " + result);
-}
-
-
-
-
